c0690034a5c844a5386170406ed8b6a2368e22e3
Maksim <craash00077@gmail.com>
2024-03-03 11:14:15 +0300

    Добавьте файлы проекта.

---
+++ DBlibrary/MainConnector.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace DBlibrary
+{
+    public class MainConnector
+    {
+
+        readonly SqlConnection connection = new SqlConnection(ConnectionString.MsSqlConnection);
+        public async Task<bool> ConnectAsync()
+        {
+            bool result;
+            try
+            {
+                await connection.OpenAsync();
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                result = false;
+            }
+            return result;
+
+
+        }
+
+        public async void DisconnectAsync()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                await connection.CloseAsync();
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                return connection;
+            }
+            else
+            {
+                throw new Exception("Connection closed");
+            }
+        }
+    }
+
+
+}
